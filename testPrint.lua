a5d013685a07e13fdab51c5a89103a53d9758459
SummersAreShort <148835243+ShortSummer@users.noreply.github.com>
2023-10-23 20:57:40 -0700

    Update testPrint.lua

--- testPrint.lua
+++ testPrint.lua
@@ -1,38 +1,4 @@
 local t = {
   ["Version"] = 1
-  ["ValidReturns"] = {}
-}
-
-local alphabet = {}
-
-for i=97, 122 do
-	local char = utf8.char(i)
-	table.insert(alphabet, char)
-end
-function getRandomString(len, used)
-  local alrUsed = true
-  local randString = ""
-  repeat
-    randString = ""
-    alrUsed = false
-    for i=1, len do
-        rand = math.random(1, #alphabet)
-        randString = randString..alphabet[rand]
-        if i == len then
-          for i, v in used do
-            if v == randString then
-              alrUsed = true
-            end
-        end
-    end
-  until alrUsed == false
-  return randString
-end
-local UsedStrings = {}
-for i=1, 1000 do
-    local rande = getRandomString(60, UsedStrings)
-    table.append(t["ValidReturns"], rande)
-    table.append(UsedStrings, rande)
-end
-
+  ["Signature"] = "UNQGAKVEAS0FDM4C93FV6NZM2DE760WF98VRYPELKVKB17CV76ZJNDVMWGBQZG2KBU4BBPGSNUU2EHHATU5SWKQ87YRZ3U7QMWNX"
 return t
